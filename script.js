675c37be0cb53eb09072fc347383a3f3249f6c1b
333Kay333 <140438505+333Kay333@users.noreply.github.com>
2024-02-01 12:02:45 -0500

    #SHE music added

--- script.js
+++ script.js
@@ -22,29 +22,13 @@ let updateTimer;
 
 const music_list = [
     {
-        img : 'images/stay.png',
-        name : 'Stay',
-        artist : 'The Kid LAROI, Justin Bieber',
-        music : 'music/stay.mp3'
+        img : 'images/APL.png',
+        name : 'SHE',
+        artist : 'Alison Phoebe Lue',
+        music : 'music/APL.mp3'
     },
-    {
-        img : 'images/fallingdown.jpg',
-        name : 'Falling Down',
-        artist : 'Wid Cards',
-        music : 'music/fallingdown.mp3'
-    },
-    {
-        img : 'images/faded.png',
-        name : 'Faded',
-        artist : 'Alan Walker',
-        music : 'music/Faded.mp3'
-    },
-    {
-        img : 'images/ratherbe.jpg',
-        name : 'Rather Be',
-        artist : 'Clean Bandit',
-        music : 'music/Rather Be.mp3'
-    }
+    
+    
 ];
 
 loadTrack(track_index);
